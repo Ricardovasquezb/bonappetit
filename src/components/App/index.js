11982a11fad3868a6bcf44a0f880886f2369d481
Ricardo Andres Vasquez Brito <>
2020-02-18 10:53:17 -0400

    Components management and creation

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+const App = () => (
+  <Router>
+    <Navigation />
+  </Router>
+);
+export default App;
