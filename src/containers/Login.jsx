699205e7e67d72dbfd432a7d359e8cfeee9172d0
Edisson Mata <edisson.mata@pitech.com.do>
2020-02-19 15:13:12 -0400

    TextInput has been completed

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../components/normalButton';
-
+import Input from "../components/TextInput"
 
 const Login = props => {
+    const [username, setUsername] = useState("");
+
+    const handleUsername = e => {
+        setUsername(e.target.value);
+    }
+
     return(
         <div>
+            <Input 
+                label={"Texto de prueba"}
+                value={username}
+                change={handleUsername}
+            />
+            <Input 
+                label={"Texto de prueba"}
+                value={username}
+                change={handleUsername}
+            />
+                        
             <Button darkmode click={ () => {
                 
             } }>
