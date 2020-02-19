e29d03aa587808ecf258f679abf13a67ed7c130f
Edisson Mata <edisson.mata@pitech.com.do>
2020-02-19 15:18:15 -0400

    Text input has been integrated

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Button from '../components/normalButton';
 import TextButton from '../components/textButton';
 import '../assets/css/login.css';
+import TextInput from "../components/TextInput"
 
 const Login = props => {
     const [username, setUsername] = useState("");
@@ -12,6 +13,16 @@ const Login = props => {
 
     return(
         <div >
+            <TextInput 
+                label="Texto de prueba"
+                value={username}
+                change={handleUsername}
+            />
+            <TextInput 
+                label="Texto de prueba"
+                value={username}
+                change={handleUsername}
+            />            
             <Button darkmode click={ () => {
                 
             } }>
