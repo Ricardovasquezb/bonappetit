c349c3f3e469672d085968eab25746552542c18c
Edisson Mata <edisson.mata@pitech.com.do>
2020-02-19 09:47:04 -0400

    in process

---
+++ src/components/LayoutType1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../assets/css/layout-type-1.css"
+
+const LayoutType1 = () => {
+    return (
+        <div className="layout-type-1">
+            
+        </div>
+    );
+}
+
+export default LayoutType1;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./assets/css/index.css"
+
+ReactDOM.render(
+    <div>xd</div>,
+    document.getElementById('root')
+);
