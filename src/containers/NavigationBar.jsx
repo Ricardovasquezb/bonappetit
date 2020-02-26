d0721ffc577139d5ca9ba34ca833ad6d8e3e2e10
ricardovasquezb <ricardovasquezbrito@gmail.com>
2020-02-26 13:19:57 -0400

    begining top navigation bar

---
+++ src/containers/NavigationBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useHistory } from "react-router-dom"
+import "../assets/css/navigation-bar.css"
+import { render } from '@testing-library/react'
+
+const Navigationbar = props => {
+
+    let pages = [
+        {label: 'Inicio', link: '/'}
+    ];
+    return(
+        <div>
+            
+        </div>
+        <div>
+
+        </div>
+    )
+}
+
+export default NavigationBar
