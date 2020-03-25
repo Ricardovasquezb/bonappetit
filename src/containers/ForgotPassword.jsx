99e262dffaef9fb9936fd98832e237a81ade9833
Ricardo Andres Vasquez Brito <ricardovasquezbrito@gmail.com>
2020-03-25 12:33:11 -0400

    forgot password added and working

--- src/containers/ForgotPassword.jsx
+++ src/containers/ForgotPassword.jsx
@@ -5,9 +5,14 @@ import TextInput from '../components/TextInput'
 import { useHistory } from "react-router-dom"
 import Card from '../components/Card'
 import Text from '../components/Text'
+import sweetalert from 'sweetalert'
+import firebaseContext from "../hooks/firebaseContext"
+
+
 
 const ForgotPassword = props => {
 
+    const Consumer = firebaseContext.Consumer
 
     const [email, setEmail] = useState("");
     const history = useHistory();
@@ -15,28 +20,50 @@ const ForgotPassword = props => {
     const handleEmail = e => {
         setEmail(e.target.value);
     }
+    return(
+        <Consumer>
+            {
+                contextResult=>{
+                 //const firebase = contextResult.firebaseAppAuth;
+                 const firebase = {
+                     appAuth: ()=> contextResult.firebaseAppAuth,
 
-    return (
-        <Card mode='darkmode-forgotPassword'>
-            <Text
-                children = "Ingrese su correo electrónico para poder comprobar su identidad"
-            />
+                 };
 
-            
-            <TextInput text-input
-                label="Correo Electrónico"
-                value = {email}
-                change = {handleEmail}
-            />   
+                return (
+                    <Card mode='darkmode-forgotPassword'>
+                        <Text
+                            children = "Ingrese su correo electrónico para poder comprobar su identidad"
+                        />
 
-            <Button darkmode click = { ()=>{
+                        
+                        <TextInput text-input
+                            label="Correo Electrónico"
+                            value = {email}
+                            change = {handleEmail}
+                        />   
 
+                        <Button darkmode click = { ()=>{
 
-            } }>
-                Recuperar Contraseña
-            </Button>
-        </Card>
+                            firebase.appAuth().sendPasswordResetEmail(email)
+                            .then((result)=>{
+                                console.log(result)
+                                history.replace("/login")
+
+                            })
+
+                            .catch((e)=>{
+                                console.log(e)
+                            })
+                        } }>
+                            Recuperar Contraseña
+                        </Button>
+                    </Card>
+                )
+                }
+
+            }
+        </Consumer>
     )
 }
-
 export default ForgotPassword
