2e7ea44dfafd2db849409e1832f2c3d613569e75
Joracaam <36743067+Joracaam@users.noreply.github.com>
2020-04-02 00:31:13 -0400

    Home and Image Cards

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,7 +3,9 @@ import { useHistory } from "react-router-dom"
 import "../assets/css/home.css"
 import CarouselView from '../components/CarouselView'
 import CardView from '../components/CardView'
+import LayoutType1 from '../components/LayoutType1'
 import image from '../assets/img/Logo_Fondo_Blanco.png'
+
 
 const Home = props =>{
     
@@ -13,56 +15,81 @@ const Home = props =>{
         setUserName(e.target.value);
     }
 
+    var Slider_Source = [
+        {
+            'src': 'http://www.singlecolorimage.com/get/33fd8f/700x350',
+            'label': 'Bienvenido a Bon APPetit',
+            'description': 'Para realizar una reservacion seleccionesun restaurant'
+        },
+        {
+            'src': 'http://www.singlecolorimage.com/get/ff85a9/700x350',
+            'label': 'Te gustan las ofertas?',
+            'description': 'Bon APPetit te permite visualizar las ofertas de un restaurante y reservar para una de estas'
+        },
+        {
+            'src': 'http://www.singlecolorimage.com/get/41427a/700x350',
+            'label': '¡Estés donde estés!',
+            'description': 'Te ofrecemos la posibilidad de llevar a cabo tus reservaciones para el restaurant'
+        }
+    ];
+
     const cards_values = [
         {
-            // 'src': {image},
+            'src': 'https://images2.listindiario.com/image/article/339/680x460/0/D4D01D5D-11C3-4153-A15F-74095C9212E1.jpeg',
             'color': 'Dark',
-            'title': 'Meson de la cava',
-            'content': 'Noche de parejas',
+            'title': 'Casa Avila',
+            'content': 'Viernes Familiar',
             'href' : 'login',
-            'link' : 'login',
+            'ButtonLabel': 'Reservar',
+        },
+        {
+            'src': 'https://www.chtmagazine.com/wp-content/uploads/2019/01/sabor.png',
+            'color': 'Dark',
+            'title': 'Del Mar a la mesa',
+            'content': 'Tarde de parejas',
+            'ButtonLabel': 'Reservar',
+        },
+        {
+            'src': 'https://megusta.do/storage/32000/8228/1811d7aee9d79e93dba476897c38b2cb.jpg',
+            'color': 'Dark',
+            'title': 'Borbone',
+            'content': 'Comida Italiana',
             'ButtonLabel': 'Reservar'
         },
         {
-            // 'src': {image},
+            'src': 'https://hoy.com.do/wp-content/uploads/2017/07/21_07_2017-HOY_VIERNES_210717_-%C2%A1Vivir1-C-653x450.jpg',
             'color': 'Dark',
             'title': 'Meson del Iberia',
-            'content': 'Tarde de parejas',
+            'content': 'Noche de parejas',
             'ButtonLabel': 'Reservar'
         },
         {
-            // 'src': {image},
+            'src': 'https://images2.listindiario.com/image/article/339/680x460/0/D4D01D5D-11C3-4153-A15F-74095C9212E1.jpeg',
             'color': 'Dark',
-            'title': 'Meson del Iberia',
+            'title': 'Casa Avila',
+            'content': 'Viernes Familiar',
+            'href' : 'login',
+            'ButtonLabel': 'Reservar',
+        },
+        {
+            'src': 'https://www.chtmagazine.com/wp-content/uploads/2019/01/sabor.png',
+            'color': 'Dark',
+            'title': 'Del Mar a la mesa',
             'content': 'Tarde de parejas',
+            'ButtonLabel': 'Reservar',
+        },
+        {
+            'src': 'https://megusta.do/storage/32000/8228/1811d7aee9d79e93dba476897c38b2cb.jpg',
+            'color': 'Dark',
+            'title': 'Borbone',
+            'content': 'Comida Italiana',
             'ButtonLabel': 'Reservar'
         },
         {
-            // 'src': {image},
+            'src': 'https://hoy.com.do/wp-content/uploads/2017/07/21_07_2017-HOY_VIERNES_210717_-%C2%A1Vivir1-C-653x450.jpg',
             'color': 'Dark',
             'title': 'Meson del Iberia',
-            'content': 'Tarde de parejas',
-            'ButtonLabel': 'Reservar'
-        },
-        {
-            // 'src': {image},
-            'color': 'Dark',
-            'title': 'Meson del Iberia',
-            'content': 'Tarde de parejas',
-            'ButtonLabel': 'Reservar'
-        },
-        {
-            // 'src': {image},
-            'color': 'Dark',
-            'title': 'Meson del Iberia',
-            'content': 'Tarde de parejas',
-            'ButtonLabel': 'Reservar'
-        },
-        {
-            // 'src': {image},
-            'color': 'Dark',
-            'title': 'Meson del Iberia',
-            'content': 'Tarde de parejas',
+            'content': 'Noche de parejas',
             'ButtonLabel': 'Reservar'
         }
     ];
@@ -71,21 +98,14 @@ const Home = props =>{
     return(
         <div className='home'>
 
-            <h1>Welcome to the Home!</h1>
-            {/* <CardView src={image} color='Secondary' title='Meson de la cava' content='Noche de parejas' ButtonLabel='Reservar'/> */}
-            
+            <div className='home-twocol'>
+                <CarouselView className='home-col1'
+                        source={Slider_Source}
+                />
+                <p>Hola</p>
+            </div>
             <CardView values={cards_values}/>
            
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
-            </p>
-            
-             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
-            </p>
-            
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam nihil temporibus nulla voluptate dolor enim, molestiae, atque corporis corrupti ab deleniti voluptatem. Enim numquam, doloremque a vitae hic reiciendis nesciunt!
