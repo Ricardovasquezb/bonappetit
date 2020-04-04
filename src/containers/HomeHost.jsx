5e5375639bc6135c9ead3df50572a5085d42c15d
Joracaam <joracaam@gmail.com>
2020-04-04 19:39:33 -0400

    Cambios en el Footer

---
+++ src/containers/HomeHost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/home-host.css';
+
+const HomeHost = props =>{
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default HomeHost;
