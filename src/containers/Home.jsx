653606b7a3f767a18f7f6a7fa6168f079f86a93b
Ricardo Andres Vasquez Brito <ricardovasquezbrito@gmail.com>
2020-04-06 15:41:53 -0400

    added user name to the home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -20,17 +20,17 @@ const Home = props =>{
 
     var Slider_Source = [
         {
-            'src': 'http://www.singlecolorimage.com/get/33fd8f/400x250',
-            'label': 'Bienvenido a Bon APPetit',
-            'description': 'Para realizar una reservacion seleccionesun restaurant'
+            'src': 'http://www.singlecolorimage.com/get/33fd8f/200x50',
+            'label': `${User.name}, esto es Bon APPetit`,
+            'description': 'Para realizar una reservacion selecciones un restaurant'
         },
         {
-            'src': 'http://www.singlecolorimage.com/get/ff85a9/400x250',
-            'label': 'Te gustan las ofertas?',
+            'src': 'http://www.singlecolorimage.com/get/ff85a9/200x50',
+            'label': '¿Te gustan las ofertas?',
             'description': 'Bon APPetit te permite visualizar las ofertas de un restaurante y reservar para una de estas'
         },
         {
-            'src': 'http://www.singlecolorimage.com/get/41427a/400x250',
+            'src': 'http://www.singlecolorimage.com/get/6ea1d2/200x50',
             'label': '¡Estés donde estés!',
             'description': 'Te ofrecemos la posibilidad de llevar a cabo tus reservaciones para el restaurant'
         }
