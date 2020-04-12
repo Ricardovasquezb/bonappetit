f4d611541ce3c44ac1d7e974bb63cd2eaead48fe
Joracaam <joracaam@gmail.com>
2020-04-12 17:01:26 -0400

    PageNotFound

--- src/containers/ContainerNotFound.jsx
+++ src/containers/ContainerNotFound.jsx
@@ -9,7 +9,7 @@ const PageNotFound = () => (
   <div className='content-not-found'>
         {/* Mensaje cuando llegue a página incorrecta */}
         {/* <img src="https://webstockreview.net/images/planet-clipart-el-espacio-16.png" alt="" width="400"/> */}
-        <h2>Uy, llegaste a un mundo desconocido. Mejor regresa al <strong>tuyo</strong>.</h2>
+        <h2>Uy, llegaste a un mundo desconocido. Mejor regresa al tuyo.</h2>
         <div>
             <Button variant="outline-success" href="#home">ir a Inicio</Button>
         </div>
